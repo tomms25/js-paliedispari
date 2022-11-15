584cb3f1d8931bd637d27402540a2e0988aec838
tomms25 <tommasobaccano@gmail.com>
2022-11-15 18:49:09 +0100

    Esercizi terminati

--- Palindromi/js/main.js
+++ Palindromi/js/main.js
@@ -1,43 +1,30 @@
 // Chiedere all’utente di inserire una parola
 
-const parolaUtente = parseInt(prompt("Scopri se la parola che hai scelto è palindroma"))
+// Creare una funzione per capire se la parola inserita è palindroma. 
 
-console.log(parolaUtente);
+// Dare output relativo.
 
-// Creare una funzione per capire se la parola inserita è palindroma
+const parolaImmessa = prompt('Inserisci una parola');
 
-
-//  let parolaDivisa = parolaUtente.split("");
-//  console.log(parolaDivisa);
-
-//  parolaDivisa = parolaDivisa.reverse();
-//  console.log(parolaDivisa);
-
-//  let parolaAlContrario = parolaDivisa.join("");
-//  console.log(parolaAlContrario);
+// console.log("La tua parola è:" + " " + parolaImmessa);
 
 
-function parolaAlContrario(parola) {
-    let parolaEndStart = parola.split("");
-    parolaEndStart = parolaEndStart.reverse();
-    parolaEndStart = parolaEndStart.join("");
-    return parolaEndStart;
-}
+// Devo verificare se la parola iniziale e quella invertita combaciano 
+const parolaUtenteInvertita = invertiParola(parolaImmessa);
 
-function palindroma(parola, parolaReverse) {
-    return parolaUtente == parolaCapovolta;
-}
-
-
-
-
-let parolaCapovolta = parolaAlContrario(parolaUtente);
-console.log(parolaCapovolta);
-
-if (palindroma(parolaUtente, parolaCapovolta)) {
-    console.log("Esatto la parola è palindroma");
-    document.getElementById("parola_scelta").innerHTML = "Esatto la parola è palindroma"
+if(parolaImmessa === parolaUtenteInvertita){
+  console.log('la parola è palindroma');
 } else {
-    console.log("Sbagliato! La parola che hai inserito non è palindroma");
-    document.getElementById("parola_scelta").innerHTML = "Sbagliato! La parola che hai inserito non è palindroma"
+  console.log('la parola non è palindroma');
+}
+
+
+// Con una funzione cerco di verificare se la parola che l'utente inserisce è effettivamente palindroma
+// Uso split per dividere la parole, reverse per invertirla e join per riunirla
+
+
+
+function invertiParola(palindroma) {
+  const parolaInvertita = palindroma.split('').reverse().join('');
+  return parolaInvertita;
 }
--- Pari e dispari /js/main.js
+++ Pari e dispari /js/main.js
@@ -1,7 +1,63 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+// Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). 
+// Dichiariamo chi ha vinto.
+
+// Inizio definendo i prompt:
+
+// Per pari e dispari 
+
+const opzione = prompt("Scegli pari o dispari");
+console.log('Hai scelto', opzione);
+
+// Per i numeri da 1 a 5
+const numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log('Il numero scelto è', numeroUtente);
+
+
+const numeroPc = numRandom();
+
+console.log('Il numero del pc è', numeroPc);
+
+// Devo fare la somma tra il numero scelto dall'utente e quello del pc per capire se il risultato sia pari o dispari
+let somma = numeroUtente + numeroPc;
+console.log('La somma dei due numeri è', somma);
+
+
+let risultato = PariODispari(somma);
+
+// In caso di vittoria avrò
+
+if (opzione === "pari" && risultato === true){
+  console.log("Hai vinto!");
+} 
+else if (opzione === "dispari" && risultato === false){
+  console.log("Hai vinto!")
+}
+// In caso di sconfitta avrò
+ else {
+  console.log("Hai perso, riprova");
+}
 
 
 
+// Scrivo le funzioni generando un numero random da 1 a 5
 
+function numRandom(min, max) {
+    // Devo definire i valori di min e massimo, che saranno 1 e 5
+  min = 1; 
+  max = 5;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+// Scrivo la funzione per capire se il risultato è pari o dispari
 
-
+function PariODispari(pari) {
+  if (pari % 2 === 0) {
+    console.log("il risultato è pari");
+    return true;
+  } else {
+    console.log("il risultato è dispari");
+    return false;
+  }
+}
